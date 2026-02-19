f00cde42cb4afa7767dd9f0e9d2133e7735366c5
Muhammed-Anees-P <aneespengad4447@gmail.com>
2026-02-19 15:16:03 +0530

    fix(Navbar): update SVG attributes to camelCase

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -138,9 +138,9 @@ export default function Navbar() {
               viewBox="0 0 24 24"
               fill="none"
               stroke="white"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             >
               <path d="M10.268 21a2 2 0 0 0 3.464 0" />
               <path d="M11.68 2.009A6 6 0 0 0 6 8c0 4.499-1.411 5.956-2.738 7.326A1 1 0 0 0 4 17h16a1 1 0 0 0 .74-1.673c-.824-.85-1.678-1.731-2.21-3.348" />
