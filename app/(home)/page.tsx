98b3928cbbbe7c3e9d25d15078221534d6c02948
Muhammed-Anees-P <aneespengad4447@gmail.com>
2026-02-19 12:47:05 +0530

    feat(home): add home page with navbar
    - create new home page component at app/(home)/page.tsx
    - include Navbar component in the home page
    - set up basic structure for the home page

---
+++ app/(home)/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/Navbar";
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <main>
+      <Navbar />
+    </main>
+  );
+};
+
+export default HomePage;
