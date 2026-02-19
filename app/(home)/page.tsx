883372f3a2b32555da274b8c5b41e51633aeb04f
Muhammed-Anees-P <aneespengad4447@gmail.com>
2026-02-19 15:04:58 +0530

    feat(home): add hero slider component
    - integrate HeroSlider component into the home page
    - enhance user experience with a visually engaging hero section

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,3 +1,4 @@
+import HeroSlider from "@/components/HeroSlider";
 import Navbar from "@/components/Navbar";
 import React from "react";
 
@@ -5,6 +6,7 @@ const HomePage = () => {
   return (
     <main>
       <Navbar />
+      <HeroSlider/>
     </main>
   );
 };
