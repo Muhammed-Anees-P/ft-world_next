00207d56318f60df7e63da87829c47448e2bf50d
Muhammed-Anees-P <aneespengad4447@gmail.com>
2026-02-20 11:24:57 +0530

    feat(product): add product page - create new page to display available products - integrate navbar, products section, and footer components

---
+++ app/product/page.tsx
@@ -0,0 +1,15 @@
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import ProductsSection from "@/components/products/ProductsSection";
+
+const ProductPage = () => {
+  return (
+    <main>
+      <Navbar />
+      <ProductsSection />
+      <Footer />
+    </main>
+  );
+};
+
+export default ProductPage;
