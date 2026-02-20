085cf0fb2a0f23dee4f223ec0f6c8399181ceb68
Muhammed-Anees-P <aneespengad4447@gmail.com>
2026-02-20 10:21:27 +0530

    feat(pages): add about page - create dedicated page for 'About Us' content - integrate Navbar, AboutSection, and Footer components

---
+++ app/about/page.tsx
@@ -0,0 +1,16 @@
+import AboutSection from "@/components/about/AboutSection";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <main>
+      <Navbar />
+      <AboutSection />
+      <Footer />
+    </main>
+  );
+};
+
+export default AboutPage;
