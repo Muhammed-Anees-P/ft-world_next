e8ea880244f8256ef73ed9b274fb4b5c5fe26e1b
Muhammed-Anees-P <aneespengad4447@gmail.com>
2026-02-20 12:03:40 +0530

    feat(contact-page): add contact us page
    - create new page component for contact information
    - integrate navbar, contact page component, and footer

---
+++ app/contact/page.tsx
@@ -0,0 +1,16 @@
+import ContactPage from "@/components/contact/ContactPage";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import React from "react";
+
+const Contact = () => {
+  return (
+    <main>
+      <Navbar />
+      <ContactPage />
+      <Footer />
+    </main>
+  );
+};
+
+export default Contact;
