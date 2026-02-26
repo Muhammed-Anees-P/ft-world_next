fcff683453c9bfb85fdd5310ac15249b0c7cb268
Muhammed-Anees-P <aneespengad4447@gmail.com>
2026-02-26 12:26:29 +0530

    refactor(productStore): centralize product type definitions
    - remove redundant local product and category interface definitions
    - import IProduct from shared types module for consistent type usage
    - improve code maintainability and reduce type duplication

--- store/productStore.ts
+++ store/productStore.ts
@@ -1,25 +1,9 @@
+import { IProduct } from "@/types/IProducts;";
 import { create } from "zustand";
 
-interface Category {
-  _id: string;
-  name: string;
-}
-
-interface Product {
-  _id: string;
-  name: string;
-  slug: string;
-  description?: string;
-  price: number;
-  stock: number;
-  category: Category;
-  images: string[];
-  isActive: boolean;
-}
-
 interface ProductStore {
-  products: Product[];
-  setProducts: (products: Product[]) => void;
+  products: IProduct[];
+  setProducts: (products: IProduct[]) => void;
 }
 
 export const useProductStore = create<ProductStore>((set) => ({
