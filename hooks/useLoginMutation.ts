6332a10e879a17d593e9ce3711cde402ece35ef1
Muhammed-Anees-P <aneespengad4447@gmail.com>
2026-02-27 16:07:47 +0530

    feat(auth): add login mutation hook - implement a new hook `useLoginMutation` for user authentication - utilize `@tanstack/react-query` to manage the login mutation state - update the authentication store with the access token upon successful login

---
+++ hooks/useLoginMutation.ts
@@ -0,0 +1,16 @@
+import { loginUser } from "@/services/authService";
+import { useAuthStore } from "@/store/useAuthStore";
+import { IAuthResponse, ILoginPayload } from "@/types/IRegister";
+import { useMutation } from "@tanstack/react-query";
+
+export const useLoginMutation = () => {
+  const setAuth = useAuthStore((state) => state.setAuth);
+
+  return useMutation<IAuthResponse, Error, ILoginPayload>({
+    mutationFn: loginUser,
+
+    onSuccess: (data) => {
+      setAuth(data.accessToken);
+    },
+  });
+};
