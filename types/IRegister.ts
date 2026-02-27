efad06efb9c11320021df5c5ffce56858ff87573
Muhammed-Anees-P <aneespengad4447@gmail.com>
2026-02-27 16:07:35 +0530

    feat(auth): add login and authentication types - introduce ILoginPayload for user login requests - define IUser interface for user data structure - add IAuthResponse interface for authentication responses

--- types/IRegister.ts
+++ types/IRegister.ts
@@ -6,3 +6,26 @@ export interface IRegisterPayload {
   //   address: string;
   password: string;
 }
+
+export interface ILoginPayload {
+  email?: string;
+  phone?: string;
+  password: string;
+}
+
+export interface IUser {
+  _id: string;
+  firstName: string;
+  lastName: string;
+  email?: string;
+  phone?: string;
+  //   roles: string[];
+  //   createdAt: string;
+  //   updatedAt: string;
+}
+
+export interface IAuthResponse {
+  user: IUser;
+  accessToken: string;
+  refreshToken?: string;
+}
