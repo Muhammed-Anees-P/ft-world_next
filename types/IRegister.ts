a219ddcd0999c25ed2e3c53769de015586d9f22b
Muhammed-Anees-P <aneespengad4447@gmail.com>
2026-03-02 13:26:19 +0530

    refactor(types): move IUser interface
    - relocate `IUser` interface from `IRegister.ts` to `IUser.ts`
    - improve type definition organization and separation of concerns

--- types/IRegister.ts
+++ types/IRegister.ts
@@ -1,3 +1,5 @@
+import { IUser } from "./IUser";
+
 export interface IRegisterPayload {
   firstName: string;
   lastName: string;
@@ -11,17 +13,6 @@ export interface ILoginPayload {
   email?: string;
   phone?: string;
   password: string;
-}
-
-export interface IUser {
-  _id: string;
-  firstName: string;
-  lastName: string;
-  email?: string;
-  phone?: string;
-  //   roles: string[];
-  //   createdAt: string;
-  //   updatedAt: string;
 }
 
 export interface IAuthResponse {
