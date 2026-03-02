6904de0dce59909883c9ef24479f8ee5dd96f2b7
Muhammed-Anees-P <aneespengad4447@gmail.com>
2026-03-02 13:26:36 +0530

    feat(auth): implement persistent user session management
    - persist user data in local storage alongside access token
    - add initializeAuth to load session from local storage on app start
    - enhance type safety by using IUser for user state

--- store/useAuthStore.ts
+++ store/useAuthStore.ts
@@ -1,29 +1,47 @@
+import { IUser } from "@/types/IUser";
 import { create } from "zustand";
 
 interface AuthState {
-  user: any | null;
+  user: IUser | null;
   accessToken: string | null;
-  setAuth: (token: string) => void;
+  setAuth: (token: string, user: IUser) => void;
   logout: () => void;
+  initializeAuth: () => void;
 }
 
 export const useAuthStore = create<AuthState>((set) => ({
   user: null,
   accessToken: null,
 
-  setAuth: (token) => {
+  setAuth: (token, user) => {
     localStorage.setItem("access_token", token);
+    localStorage.setItem("user", JSON.stringify(user));
 
     set({
       accessToken: token,
+      user,
     });
   },
 
   logout: () => {
     localStorage.removeItem("access_token");
+    localStorage.removeItem("user");
 
     set({
       accessToken: null,
+      user: null,
     });
+  },
+
+  initializeAuth: () => {
+    const token = localStorage.getItem("access_token");
+    const user = localStorage.getItem("user");
+
+    if (token && user) {
+      set({
+        accessToken: token,
+        user: JSON.parse(user),
+      });
+    }
   },
 }));
